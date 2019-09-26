88e5b98a0a44fe154051a9629e8eed80a12d0a76
Yakon Bagatulj <ybagatulj@cosapi.com.pe>
2019-09-26 13:08:33 -0500

    conexion y desconexion

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
 
-const publicPath = path.join(__dirname,'/../public/view');
+const publicPath = path.join(__dirname, '/../public/view');
 const port = process.env.PORT || 8002
-var app = express();
-
+let app = express();
+let server = http.createServer(app);
+let io = socket(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
-    console.log('Server is up on port ${port}');
+io.on('connection', (socket) => {
+    console.log("El nuevo usuario conectado");
+
+    socket.on('disconnect', () => {
+        console.log("Desconectado del servidor.");
+    })
+});
+
+io.on('disconnect', () => {
+    console.log("Usuario fue desconectado.");
 })
+
+server.listen(port, () => {
+    console.log("Server is up on port " + port);
+});
